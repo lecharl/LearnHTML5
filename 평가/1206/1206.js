baa47c56188a6d0964f946b7b6dc7ed5f6085b02
lecharl <lecharlhi@gmail.com>
2021-12-08 02:27:11 +0900

    navbar

--- 평가/1206/1206.js
+++ 평가/1206/1206.js
@@ -1,18 +1,36 @@
-let submit = document.getElementById('signIn');
-let id = document.getElementById('userId');
-let pwd1 = document.getElementById("userPwd1");
-let pwd2 = document.getElementById("userPwd2");
+const submit = document.getElementById('signIn');
+const id = document.getElementById('userId');
+const pwd1 = document.getElementById("userPwd1");
+const pwd2 = document.getElementById("userPwd2");
+const email = document.getElementById("email");
 let pil1 = document.getElementById("pil1");
 let pil2 = document.getElementById("pil2");
+let pil2_2 = document.getElementById("pil2_2");
 let pil3 = document.getElementById("pil3");
 
-function bluring(any) {
-	if (any.value.length == 0) {
-		pil1.innerHTML = "필수 입력 항목 입니다";
-		alert("Please enter");
+//요구사항2
+//이메일 클릭했을 때,
+//앞의 id input 박스 중 빈칸이 있다면??
+//그 칸 밑에다가 '필수입력' 이라고 표시하기
+
+// email.addEventListener('click', function () {
+// 	if (id.value === "") {
+// 		pil1.innerHTML = "필수 입력 항목 입니다";
+// 	} else {
+// 		pil1.innerHTML = "";
+// 	}
+// })
+
+
+
+function bluring(input, out) {
+	if (input.value.length == 0) {
+		out.innerHTML = `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;필수 입력 항목 입니다`;
 		return false;
+	} else {
+		out.innerHTML = "";
+		return true;
 	}
-	return true;
 }
 
 pwd1.addEventListener("click", function () {
